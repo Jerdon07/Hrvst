ff077173652006c98fdb4cb8067c455b6791cd04
Jerdon07 <jdlitaoen@gmail.com>
2025-12-26 22:28:12 +0800

    feat: Add show component file for showing a single crop

---
+++ resources/js/pages/admin/crops/show.jsx
@@ -0,0 +1,14 @@
+
+
+import AdminLayout from "@/layouts/admin-layout"
+
+export default function Show({  }) {
+
+    return (
+        <AdminLayout
+            title='Crop'
+        >
+            Crop
+        </AdminLayout>
+    )
+}
