4b9e775c59af173ad103c10c058989c29179effc
Jerdon07 <jdlitaoen@gmail.com>
2025-12-26 22:22:58 +0800

    refactor: lot of fixes

--- app/Http/Controllers/Admin/AdminCropController.php
+++ app/Http/Controllers/Admin/AdminCropController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Crop;
+use App\Models\CropPrice;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
@@ -13,11 +15,63 @@ class AdminCropController extends Controller
 {
     public function index()
     {
-        $crops = Crop::with('category')->get();
+        $crops = Crop::with([
+            'category',
+            'latestPrice:id,crop_id,price_min,price_max,recorded_at'
+        ])->get();
 
         return Inertia::render('admin/crops/index', [
             'crops' => $crops,
         ]);
+    }
+
+    
+
+    public function create()
+    {
+        $categories = Category::all();
+        
+        return Inertia::render('admin/crops/create', [
+            'categories' => $categories,
+        ]);
+    }
+     
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            /* Crop Data */
+            'name' => 'required|string|max:255',
+            'category_id' => 'required|exists:categories,id',
+            'image_path' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'crop_weeks' => 'required|numeric|min:1|max:356',
+            /* Price Data */
+            'price_min' => 'required|numeric|min:0|max:999.99|lte:price_max',
+            'price_max' => 'required|numeric|min:0|max:999.99|gte:price_min',
+            
+        ]);
+
+        try {
+            DB::transaction(function () use ($validated) {
+                /* Create crop */
+                $crop = Crop::create([
+                    'name' => $validated['name'],
+                    'category_id' => $validated['category_id'],
+                    'image_path' => $validated['image_path']->store('crops', 'public'),
+                    'crop_weeks' => $validated['crop_weeks'],
+                ]);
+                CropPrice::create([
+                    'crop_id' => $crop->id,
+                    'price_min' => $validated['price_min'],
+                    'price_max' => $validated['price_max'],
+                    'recorded_at' => now()->toDateString(),
+                ]);
+            });
+        } catch (\throwable $e) {
+            dd($e->getMessage());
+        }
+        
+
+        return redirect()->route('admin.crops.index')->with('success', 'Crop created successfully');
     }
 
     public function show(Crop $crop)
@@ -27,28 +81,11 @@ class AdminCropController extends Controller
         ]);
     }
 
-    public function create(Request $request, Crop $crop)
+    public function edit(Crop $crop)
     {
-        $validate = $request->validate([
-            'name' => 'required|string|max:255',
-            'low_price' => 'required|numeric|min:0|max:999999.99',
-            'high_price' => 'required|numeric|min:0|max:999999.99|gte:low_price',
-            'harvest_weeks' => 'required|integer|min:1|max:52',
-            'category_id' => 'required|exists:categories,id',
-            'image_path' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        return Inertia::render('admin/crops/edit', [
+            'crop' => $crop,
         ]);
-
-        if ($request->hasFile('image_path')) {
-            $path = $request->file('image_path')->store('crops', 'public');
-            $validate['image_path'] = $path;
-        }
-
-        $crop->create($validate);
-
-
-
-        return redirect()->route('crops.index')
-            ->with('success', 'Crop created successfully.');
     }
 
     public function update(Request $request, Crop $crop)
@@ -77,28 +114,7 @@ class AdminCropController extends Controller
             ->with('success', 'Crop updated successfully.');
     }
 
-    public function store(Request $request)
-    {
-        $validated = $request->validate([
-            'category_id' => 'required|exists:categories,id',
-            'name' => 'required|string|max:255',
-            'price_min' => 'required|numeric|min:0|max:999999.99',
-            'price_max' => 'required|numeric|min:0|max:999999.99|gte:low_price',
-            'crop_weeks' => 'required|integer|min:1|max:52',
-            'recorded_at' => 'required|date',
-            'image_path' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-        ]);
-
-        if ($request->hasFile('image_path')) {
-            $path = $request->file('image_path')->store('crops', 'public');
-            $validated['image_path'] = $path;
-        }
-
-        Crop::create($validated);
-
-        return redirect()->route('crops.index')
-            ->with('success', 'Crop created successfully.');
-    }
+    
 
     public function destroy(Crop $crop)
     {
