f9259a57fbe7001a24853141d630e56f7e1d7428
Jerdon07 <jdlitaoen@gmail.com>
2025-12-27 21:56:25 +0800

    feat: Add seeders for weekly crop prices

--- database/seeders/CropPricesSeeder.php
+++ database/seeders/CropPricesSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Crop;
+use App\Models\CropPrice;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,26 @@ class CropPricesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $crops = Crop::all();
+
+        foreach ($crops as $crop) {
+            $date = now()->startOfWeek()->subWeeks(8);
+            
+            for ($i = 0; $i < 8; $i++) {
+                CropPrice::updateOrCreate(
+                    [
+                        'crop_id' => $crop->id,
+                        'recorded_at' => $date,
+                    ],
+                    [
+                        'price_min' => rand(20, 50),
+                        'price_max' => rand(60, 100),
+                    ]
+                );
+
+                $date->addWeek();
+            }
+
+        }
     }
 }
