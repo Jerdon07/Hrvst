ea9c1795fb59e31d0eb7bf08548d26d6fe7d14ba
Jerdon07 <jdlitaoen@gmail.com>
2025-12-29 10:11:27 +0800

    feat: Add index method

--- app/Http/Controllers/Admin/AdminDemoController.php
+++ app/Http/Controllers/Admin/AdminDemoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Municipality;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,6 +11,32 @@ class AdminDemoController extends Controller
 {
     public function index()
     {
-        return Inertia::render('admin/farmers/demographics/index');
+        $municipalities = Municipality::withCount('farmers')->get()
+            ->map(function ($municipality) {
+                return [
+                    'id' => $municipality->id,
+                    'name' => $municipality->name,
+                    'farmer_count' => $municipality->farmers_count
+                ];
+            });
+        
+        $barangays = Municipality::with(['barangays' => function ($query) {
+            $query->withCount('farmers');
+        }])->get()->mapWithKeys(function ($municipality) {
+            return [
+                $municipality->id => $municipality->barangays->map(function ($barangay) {
+                    return [
+                        'id' => $barangay->id,
+                        'name' => $barangay->name,
+                        'farmer_count' => $barangay->farmers_count,
+                    ];
+                }),
+            ];
+        });
+
+        return Inertia::render('admin/farmers/demographics/index', [
+            'municipalities' => $municipalities,
+            'barangays' => $barangays,
+        ]);
     }
 }
