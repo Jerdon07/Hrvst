74774feb11bad17052b9beb02043b8a83c1e52c1
Jerdon07 <jdlitaoen@gmail.com>
2025-12-29 10:09:22 +0800

    feat: Add file for indexing crops prices

---
+++ resources/js/pages/admin/prices/index.jsx
@@ -0,0 +1,9 @@
+import AdminLayout from "@/layouts/admin-layout";
+
+export default function Prices() {
+    return (
+        <AdminLayout>
+            Prices Comparison
+        </AdminLayout>
+    )
+}
